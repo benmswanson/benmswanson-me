6ef6dd25e2d7ba18fc5c1a269613de0a64dd94a7
Ben Swanson <benjaminmswanson@gmail.com>
2026-09-09 14:54:35 -0400

    Update site URL to benmswanson.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://benswanson.me',
+  site: 'https://benmswanson.me',
   adapter: cloudflare(),
 });
