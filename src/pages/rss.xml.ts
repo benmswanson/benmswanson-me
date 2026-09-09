8bd8ea43b3d4a3d24b03d26836eab36747951a59
Ben Swanson <benjaminmswanson@gmail.com>
2026-09-09 16:18:29 -0400

    Lowercase p in tagline

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -8,7 +8,7 @@ export async function GET(context: APIContext) {
 
   return rss({
     title: 'Ben Swanson',
-    description: 'Projects, interests, etc.',
+    description: 'projects, interests, etc.',
     site: context.site!,
     items: posts.map((post) => ({
       title: post.data.title,
